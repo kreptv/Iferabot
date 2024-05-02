60a3c20018706a0589b70211f97c54234b6c90ca
kreptv <kreptacat@outlook.com>
2024-05-02 01:08:09 -0500

    oops

--- mongo.js
+++ mongo.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 //const {mongoPath} = require('./config.json')
 
-const mongoPath = 'mongodb+srv://krepta:Krepta0308YC@iferabot.iexmd.mongodb.net/blobbot?retryWrites=true&w=majority'
+const mongoPath = 'mongodb+srv://krepta:password@iferabot.iexmd.mongodb.net/blobbot?retryWrites=true&w=majority'
 
 module.exports = async () => {
   await mongoose.connect(mongoPath, {
